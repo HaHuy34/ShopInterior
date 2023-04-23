5d34c8f7adaa56d35a3f20c9ed9827a0691c0c92
HuyHa <107118105+HaHuy34@users.noreply.github.com>
2023-04-23 23:29:36 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,8 +15,8 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
-  // autoplay: true,
-  // autoplayTimeout: 2000,
+  autoplay: true,
+  autoplayTimeout: 3000,
   responsive: {
     0: {
       items: 1,
