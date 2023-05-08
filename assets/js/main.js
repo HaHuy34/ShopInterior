921b2729a19b9389bafcadf934690e140d98ca52
HuyHa <107118105+HaHuy34@users.noreply.github.com>
2023-05-08 10:04:22 +0700

    Update Add To Cart click

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,12 +75,6 @@ $(".js-filter").on("click", function () {
   }
 });
 
-//Todo: Alert Add To Cart
-// const addtoCart = document.getElementsByClassName("card-item");
-// addtoCart.addEventListener("click", function(){
-//   alert("Product added to cart");
-// })
-
 // Todo: SLIDE BANNER IMAGE
 $(".slide-banner-image").slick({
   slidesToShow: 6,
@@ -99,4 +93,35 @@ $(".cooperate-main").slick({
 
 function redirect() {
   window.location.href = "product_detail.html";
+}
+
+// Todo: Add To Cart Icon Header
+var addToCartButtons = document.getElementsByClassName("add-to-cart");
+var cartCount = document.getElementById("cart-count");
+
+var itemsInCart = [];
+
+for (var i = 0; i < addToCartButtons.length; i++) {
+  addToCartButtons[i].addEventListener("click", function () {
+    var itemName = this.getAttribute("data-name");
+    var itemPrice = parseFloat(this.getAttribute("data-price"));
+    var itemExists = false;
+
+    // Kiểm tra sản phẩm đã tồn tại trong danh sách hay chưa
+    for (var j = 0; j < itemsInCart.length; j++) {
+      if (itemsInCart[j].name === itemName) {
+        itemExists = true;
+        break;
+      }
+    }
+
+    // Nếu sản phẩm chưa tồn tại, thêm sản phẩm vào danh sách
+    if (!itemExists) {
+      itemsInCart.push({ name: itemName, price: itemPrice });
+      cartCount.innerHTML = itemsInCart.length;
+      alert("Successfully added shopping carts");
+    } else {
+      alert("The product has existed in the cart");
+    }
+  });
 }
